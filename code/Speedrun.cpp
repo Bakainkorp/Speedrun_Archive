2178315b96260c226c133e391955d2a933c49c02
Bakainkorp <54682699+Bakainkorp@users.noreply.github.com>
2020-01-03 23:55:08 -0500

    Create Speedrun.cpp

---
+++ code/Speedrun.cpp
@@ -0,0 +1,17 @@
+/*
+Filename: Speedrun.cpp
+
+Purpose: Main file
+
+Note: This file is NOT finished. This is a TEMPORARY main file, and is subject to massive change.
+
+*/
+
+#include "game.cpp"
+
+using namespace std;
+
+int main() 
+{
+	return 0;
+}
