f122739015e29ac07e5f3f0779bbd3dcde39982b
Bakainkorp <54682699+Bakainkorp@users.noreply.github.com>
2020-01-05 18:34:26 -0500

    Update Speedrun.cpp

--- code/Speedrun.cpp
+++ code/Speedrun.cpp
@@ -3,16 +3,19 @@ Filename: Speedrun.cpp
 
 Purpose: Main file
 
-Note: This file is NOT finished. This is a TEMPORARY main file, and is subject to massive change.
-
 To run the file, first clone the directory into your local machine. 
 Go to the directory via Terminal or a similar command line interface.
 Type "g++ Speednrun.cpp".
 After it compiles, type "./a.out" to run the program.
 
+----Notes----
+
+- The file is able to run, but is NOT finished. This file will likely be replaced with a more appropriate one, including its file name.
+
 */
 
 #include "Function.cpp"
+#include "menu.cpp"
 
 using namespace std;
 
@@ -20,6 +23,26 @@ int main()
 {
 	vector<Game> game;
 	initialize(game);
+	bool exit = false;
+
+	while (exit == false)
+	{
+		exit = M1_Question(0);
+	}
 
 	return 0;
 }
+
+/*
+
+----Files that are directly dependent on this file----
+
+----Files this file is dependent on----
+- game.cpp
+- game.h
+- Function.h
+- Function.cpp
+- Menu.h
+- Menu.cpp
+
+*/
