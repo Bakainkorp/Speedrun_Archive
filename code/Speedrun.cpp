03a8280392e968b84542d66a138da506d8a92a76
Bakainkorp <54682699+Bakainkorp@users.noreply.github.com>
2020-01-04 20:51:53 -0500

    Update Speedrun.cpp

--- code/Speedrun.cpp
+++ code/Speedrun.cpp
@@ -5,6 +5,11 @@ Purpose: Main file
 
 Note: This file is NOT finished. This is a TEMPORARY main file, and is subject to massive change.
 
+To run the file, first clone the directory into your local machine. 
+Go to the directory via Terminal or a similar command line interface.
+Type "g++ Speednrun.cpp".
+After it compiles, type "./a.out" to run the program.
+
 */
 
 #include "Function.cpp"
