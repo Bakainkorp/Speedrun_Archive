efecc67e38d22664a7e91e11101a286652c01221
Bakainkorp <54682699+Bakainkorp@users.noreply.github.com>
2020-01-05 18:36:27 -0500

    Update Function.h

--- code/Function.h
+++ code/Function.h
@@ -3,7 +3,9 @@ Filename: Function.h
 
 Purpose: Header file for all misc. functions that are required in Speedrun.cpp
 
-Note: This file is NOT finished.
+----Notes----
+
+- The file is able to run, but is NOT finished.
 
 */
 
@@ -12,13 +14,21 @@ Note: This file is NOT finished.
 
 #include "game.cpp"
 
-//This function is a temporary measure to ensure that the getline records the name properly
 bool isName(string str);
-
-//This function is a temporary measure to ensure that the getline records the millisecond properly
 bool isMil(string str);
 
 //Step 0, Step 1, Step 2
 void initialize(vector <Game> game);
 
 #endif // FUNCTION_H
+
+/*
+
+----Files that are directly dependent on this file----
+- Function.cpp
+
+----Files this file is dependent on----
+- game.cpp
+- game.h
+
+*/
