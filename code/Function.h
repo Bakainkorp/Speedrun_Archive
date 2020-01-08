6aa97e671ae3eb3d74169bf7ff311049f4a0d0e3
Bakainkorp <54682699+Bakainkorp@users.noreply.github.com>
2020-01-08 17:28:34 -0500

    Update Function.h

--- code/Function.h
+++ code/Function.h
@@ -3,9 +3,12 @@ Filename: Function.h
 
 Purpose: Header file for all misc. functions that are required in Speedrun.cpp
 
+Date last worked on (dd-mm-yy): 06-01-20
+
 ----Notes----
 
 - The file is able to run, but is NOT finished.
+- A good number of functions (primarily the ones involving string manipulation) in this file will be obsolete much later in the development cycle, when running the program no longer requires Terminal. A good example is stringInterpreter
 
 */
 
@@ -18,7 +21,30 @@ bool isName(string str);
 bool isMil(string str);
 
 //Step 0, Step 1, Step 2
-void initialize(vector <Game> game);
+void initialize(vector <Game> &data);
+
+//Step 22, Step 13
+bool isVectorEmpty (vector <Game> data);
+
+//Step 13a, Step 5, Step 25
+bool YN_Answer (string str);
+
+string stringInterpreter (string str);
+
+//Step 5, Step 25
+Game createGame ();
+
+//Step 26
+void userCreateRun();
+
+bool isRunValid(string str, bool mil);
+bool isDateValid(string str);
+Time stringToTime(string str, bool mil);
+Date stringToDate(string str);
+
+int loadGame(string str, vector <Game> data);
+
+void listGame(vector <Game> data);
 
 #endif // FUNCTION_H
 
