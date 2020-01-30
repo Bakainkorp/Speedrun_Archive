e76a5ee1e0501dc04ec1d36509d1145c0cebf96e
Bakainkorp <54682699+Bakainkorp@users.noreply.github.com>
2020-01-29 21:07:01 -0500

    Update Function.h

--- code/Function.h
+++ code/Function.h
@@ -3,12 +3,15 @@ Filename: Function.h
 
 Purpose: Header file for all misc. functions that are required in Speedrun.cpp
 
-Date last worked on (dd-mm-yy): 06-01-20
+Date last worked on (dd-mm-yy): 29-01-20
 
 ----Notes----
 
-- The file is able to run, but is NOT finished.
-- A good number of functions (primarily the ones involving string manipulation) in this file will be obsolete much later in the development cycle, when running the program no longer requires Terminal. A good example is stringInterpreter
+- The file is able to run. It is close to completion, but at this point, optimizations are still possible.
+
+----Notable Bugs / Optimizations----
+
+- [YUIO] Any function with this tag will likely be unncessary later in development, as an executable program will likely not use command line entry
 
 */
 
@@ -17,34 +20,34 @@ Date last worked on (dd-mm-yy): 06-01-20
 
 #include "game.cpp"
 
-bool isName(string str);
-bool isMil(string str);
-
-//Step 0, Step 1, Step 2
-void initialize(vector <Game> &data);
-
-//Step 22, Step 13
-bool isVectorEmpty (vector <Game> data);
-
-//Step 13a, Step 5, Step 25
-bool YN_Answer (string str);
-
-string stringInterpreter (string str);
-
-//Step 5, Step 25
-Game createGame ();
-
-//Step 26
-void userCreateRun();
-
-bool isRunValid(string str, bool mil);
 bool isDateValid(string str);
+bool isMilStored(string str);
+bool isName(string str);
+bool isNumber(string str);
+bool isTimeValid(string str, bool mil);
+bool YN_Answer (string str);									// [YUIO] Step 13a, Step 5, Step 25
+
+Time convertTime(long double totalMilliseconds, bool mil);
 Time stringToTime(string str, bool mil);
+
+Date getToday();
 Date stringToDate(string str);
 
-int loadGame(string str, vector <Game> data);
+int loadGame(string str, vector <Game> data);				// Step 29, Step 6
+int stringToInt (string str);
 
+Run timer(bool mil, Game game);								// Step 7, Step 8
+Run userCreateRun(bool mil, Game &game);						// Step 26
+
+void initialize(vector <Game> &data);						// Step 0, Step 1, Step 2
 void listGame(vector <Game> data);
+
+string stringInterpreter (string str);						// [YUIO]
+
+Game createGame ();											//Step 5, Step 25
+
+vector <int> chronologicalOrder (vector <int> number);
+
 
 #endif // FUNCTION_H
 
